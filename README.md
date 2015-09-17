0aa7ba017c49588412256e87cd854f884e98fea7
Anatoly Dryga <anatolydryga@gmail.com>
2015-09-17 13:39:19 -0400

    add sample name to output

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ find number of reads that align to one BLAST database but not the other.
 # Usage
 
 ````
-Rscript diff_blast.R -s sample_blast.out -r reference_blast.out
+Rscript diff_blast.R -s sample_blast.out -r reference_blast.out -n sample_name
 ```
 
 Find number of reads that aligned only to reference  `reference_blast.out` 
@@ -14,4 +14,9 @@ but not to sample `sample_blast.out`   and report it to stdout.
 Files are result of running blast with corresponding databases. The first
 column should be read identifier and file is tab-separated(e.g. outfmt = 6).
 
+# Output
+Prints to std output:
+sample_name number
+
+Separator is TAB.
 
--- diff_blast.R
+++ diff_blast.R
@@ -3,11 +3,13 @@ library(argparse, quietly=TRUE)
 parser <- ArgumentParser(description="What reads aligned only to ref but not sample")
 parser$add_argument("-s", "--sample", type="character", required=TRUE)
 parser$add_argument("-r", "--reference", type="character", required=TRUE)
+parser$add_argument("-n", "--name", type="character", required=TRUE)
 
 args <- parser$parse_args()
 
 sample_file_name <- args$sample
 ref_file_name <- args$reference
+sample_name <- args$name
 
 sample <- read.delim(sample_file_name, header=FALSE)
 ref <- read.delim(ref_file_name, header=FALSE)
@@ -16,4 +18,4 @@ sample_id <- sample[, 1]
 ref_id <- ref[, 1]
 
 only_ref_id <- setdiff(ref_id, sample_id)
-write(length(only_ref_id), stdout())
+cat(sample_name, length(only_ref_id), "\n", sep="\t")
